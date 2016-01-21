96ef183f38b14636a22fa6b02c01079a124a0d25
yangdaoyan <yangdaoyan@gmail.com>
2016-01-21 23:02:38 +0800

    Add an example of SMA and plot

--- draw_SH_index.py
+++ draw_SH_index.py
@@ -9,6 +9,9 @@ import numpy as np
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt   
+import numpy as np
+import pandas as pd
+
 
 df = pd.read_excel('.\SH_1DAY_DATA.xlsx')
 
@@ -17,12 +20,30 @@ df['Date'] = df['Date'].str.slice(0,10)
 for i in range(0, len(df['Date'])):
     df['Date'].values[i]= datetime.datetime.strptime(df['Date'].values[i],'%Y-%m-%d')
 
-df.index = df.Date
+# df.index = df.Date
 
 # x=df.Date
 # y=df.Close
 
+# Moving average 
+def sma(x, n):
+    """
+    compute an n period simple moving average.
 
+    """
+    sma = pd.rolling_mean(x,n)
+    print sma
+    
+    return sma
 
-plt.plot(x,y)
+y= np.array([1,5,3,8,12,6,20,32])
+x= np.array(range(0,len(y),1))
+sma2= sma(y,2)
+sma3= sma(y,3)
+'''print x
+print type(x)
+print y
+print type(y)
+'''
+plt.plot(x,y,"ro",sma2,y,"-", sma3,y,"-")
 plt.show()
